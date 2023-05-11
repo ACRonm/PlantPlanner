a521cb4831bb44005db2f9f8d7c4ef0ab63c0dc7
Aiden <102561643+ACRonm@users.noreply.github.com>
2023-05-11 22:01:21 +1000

    Add cards using relay for android.

--- PlantPlanner/app/src/main/java/com/acronm/plantplanner/MainActivity.kt
+++ PlantPlanner/app/src/main/java/com/acronm/plantplanner/MainActivity.kt
@@ -7,15 +7,15 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.acronm.plantplanner.card.Style
+import com.acronm.plantplanner.card.Type
 import com.acronm.plantplanner.ui.theme.PlantPlannerTheme
+import com.acronm.plantplanner.card.Card
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -30,50 +30,39 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CardLayout() {
     Column(
         modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp)
+            .fillMaxSize()
+            .padding(16.dp)
     ) {
+        //text
         Text(
             text = "Plant Planner",
-            style = MaterialTheme.typography.titleLarge
+            modifier = Modifier.padding(16.dp),
+            style = MaterialTheme.typography.titleLarge,
         )
+
+
         Spacer(modifier = Modifier.height(16.dp))
         LazyColumn(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
             items(10) {
-                ElevatedCard(onClick = { /*TODO*/ }) {
-                    Column(
-                        modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(16.dp)
-                    ) {
-                        Text(
-                            text = "Plant Name",
-                            style = MaterialTheme.typography.titleMedium
-                        )
-                        Spacer(modifier = Modifier.height(8.dp))
-                        Text(
-                            text = "Plant Description",
-                            style = MaterialTheme.typography.bodyMedium
-                        )
-                        Spacer(modifier = Modifier.height(8.dp))
-                        Text(
-                            text = "Plant Date",
-                            style = MaterialTheme.typography.labelSmall
-                        )
-                }
-                }
+                Card(
+                    type = Type.Stacked,
+                    style = Style.Outlined,
+                    showIconButton = false,
+
+                )
             }
         }
     }
 }
+
+
 
 @Preview (showBackground = true)
 @Composable
